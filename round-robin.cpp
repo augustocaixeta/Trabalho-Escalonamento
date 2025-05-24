4f043d9ba17b4cf2279f342c3a2574a307235c1a
Bruno Ferreira <117854462+brunnoferreiraaa@users.noreply.github.com>
2025-05-23 22:05:53 -0300

    Update round-robin.cpp

--- round-robin.cpp
+++ round-robin.cpp
@@ -1,71 +1,70 @@
+
 #include <stdio.h>
 
-#define MAX_PROCESSOS 100
-
-int tempos_execucao[MAX_PROCESSOS];
-int tempos_restante[MAX_PROCESSOS];
-int tat[MAX_PROCESSOS];     // Turnaround Time
-int espera[MAX_PROCESSOS];  // Tempo de Espera
-
-int tempo_total = 0;
-
-void ler_dados(int n) {
-    for (int p = 0; p < n; p++) {
-        printf("P [%02d]: ", p + 1);
-        scanf("%d", &tempos_execucao[p]);
-        tempos_restante[p] = tempos_execucao[p];
+void lerProcessoTempoExecucao(int processoTemposExecucao[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Processo %d: ", i + 1);
+        scanf("%d", &processoTemposExecucao[i]);
     }
 }
 
-void round_robin(int n, int quantum) {
-    int processos_restantes = n;
-    int tempo = 0;
-    int terminou = 0;
-    int p = 0;
+void executarRoundRobin(int processoTemposExecucao[], int n, int quantum) {
+    int processosRestantes = n;
+    int tempoAnterior = 0;
+    int i = 0;                  // Índice do processo que acontecerá o rodízio (Round).
 
     printf("\nTempo Atual\tProcesso\tTempo Executado\tTempo Restante\n");
 
-    while (processos_restantes > 0) {
-        if (tempos_restante[p] > 0) {
-            int exec_time = (tempos_restante[p] >= quantum) ? quantum : tempos_restante[p];
-            int tempo_inicial = tempo;
-            int tempo_final = tempo + exec_time;
+    while (processosRestantes > 0) {
+        if (processoTemposExecucao[i] > 0) {
+            int tempoExecucao = (processoTemposExecucao[i] >= quantum) ? quantum : processoTemposExecucao[i];
+            int tempoInicio = tempoAnterior;
+            int tempoFim = tempoAnterior + tempoExecucao;
 
-            tempos_restante[p] -= exec_time;
-            tempo = tempo_final;
+            processoTemposExecucao[i] -= tempoExecucao;
+            tempoAnterior = tempoFim;
 
             printf("%d-%d\t\tP%d\t\t%d\t\t%d\n",
-                   tempo_inicial, tempo_final,
-                   p + 1,
-                   exec_time,
-                   tempos_restante[p]);
+                   tempoInicio, tempoFim,
+                   i + 1,
+                   tempoExecucao,
+                   processoTemposExecucao[i]);
 
-            if (tempos_restante[p] == 0) {
-                int tempo_espera = tempo - tempos_execucao[p];
-                tat[p] = tempo;
-                espera[p] = tempo_espera;
-                processos_restantes--;
+            // Decrementar a variável de processos restantes quanto terminar para não entrar no próximo while.
+            if (processoTemposExecucao[i] == 0) {
+                processosRestantes--;
             }
         }
 
-        p = (p + 1) % n;
+        // Isso faz com que o indice do processo (no caso "i") retorne para o valor "0" quando chegar ao valor "n".
+        // Trazendo a ideia do "Round" (rodízio).
+        i = (i + 1) % n;
     }
-
-    tempo_total = tempo;
 }
 
 int main() {
-    int n, quantum;
-
-    printf("Numero de processos: ");
+    int n;
+    printf("Numero de Processos: ");
     scanf("%d", &n);
 
-    ler_dados(n);
+    if (n <= 0) {
+        printf("Numero de processos invalido.\n");
+        return 1;
+    }
 
-    printf("Quantum: ");
+    int processoTemposExecucao[n];
+    lerProcessoTempoExecucao(processoTemposExecucao, n);
+
+    int quantum;
+    printf("Parcela do Quantum: ");
     scanf("%d", &quantum);
 
-    round_robin(n, quantum);
+    if (quantum <= 0) {
+        printf("Quantum invalido.\n");
+        return 1;
+    }
+
+    executarRoundRobin(processoTemposExecucao, n, quantum);
 
     return 0;
 }
